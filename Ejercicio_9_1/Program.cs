0574503190cab8b7f3a80bd157e9fa05b961134c
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-02-01 14:22:39 -0400

    Add project files.

---
+++ Ejercicio_9_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Ejercicio_9_1
+{
+    class Program
+    {
+        public class EstructuraTienda
+        {
+            //Ejercicio 1 capitulo 9
+            public struct TiendaItems
+            {
+                public int CodigoBarra;
+                public string NombreProducto;
+                public int CantidadProducto;
+                public double PrecioProducto;
+                public string DepartamentoProducto;
+
+                public TiendaItems(int codigoBarra, string nombreProducto, int cantidadProducto, double precioProducto, string departamentoProducto)
+                {
+                    CodigoBarra = codigoBarra;
+                    NombreProducto = nombreProducto;
+                    CantidadProducto = cantidadProducto;
+                    PrecioProducto = precioProducto;
+                    DepartamentoProducto = departamentoProducto;
+                }
+                public override String ToString()
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendFormat("Codigo:" + CodigoBarra + "\nNombre:" + NombreProducto + "\nCantidad:" + CantidadProducto
+                        + "\nPrecio:" + PrecioProducto + "\nDepartamento:" + DepartamentoProducto);
+                    return (sb.ToString());
+                }
+            }
+        }
+    }
+}
