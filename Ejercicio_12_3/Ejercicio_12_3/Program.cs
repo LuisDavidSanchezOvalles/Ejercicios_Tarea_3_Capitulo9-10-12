c8a84fdfb49eb3d181fa03f47ad89352132a8663
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-02-01 14:43:42 -0400

    Ejercicio_12_3

---
+++ Ejercicio_12_3/Ejercicio_12_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xceed.Wpf.Toolkit;
+
+namespace Ejercicio_12_3
+{
+    class Program
+    {
+
+        class Excepciones
+    {
+        //Ejercicio 3 del Capitulo 12
+        public void ExcepcionesArreglos()
+        {
+            
+            MessageBox.Show("FormatException.");
+            MessageBox.Show("IndexOutOfBoundsRange");
+            MessageBox.Show("ArrayTypeMismatchException");
+          
+        }
+
+        public void ExcepcionesStream()
+        {
+
+            MessageBox.Show("DirectoryNotFoundException");
+            MessageBox.Show("FileNotFoundException");
+            MessageBox.Show("IOException");
+           
+        }
+
+        public void ExcepcionesWriteLine()
+        {
+
+            MessageBox.Show("ExceptionToString");
+           
+        }
+    }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
